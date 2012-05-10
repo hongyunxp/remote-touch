9bdbd9a5b78e0f0193e17c6ed90c1c2e29fa51ff
n.rusitska@gmail.com <n.rusitska@gmail.com@3bd38975-8d56-cf60-c723-4810f06ec71b>
2012-05-10 12:15:44 +0000

    dummy project

---
+++ trunk/RemoteTouch/src/de/haw_hamburg/informatik/remote_touch/RemoteTouchActivity.java
@@ -0,0 +1,13 @@
+package de.haw_hamburg.informatik.remote_touch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RemoteTouchActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
