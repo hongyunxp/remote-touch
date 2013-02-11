647c5dc921ce5b3acba84ebcb2de2cb1784a25ba
n.rusitska@gmail.com <n.rusitska@gmail.com@3bd38975-8d56-cf60-c723-4810f06ec71b>
2013-02-11 23:38:09 +0000

    completely switched to CutText. Fixed some SVN Properties

--- trunk/RemoteDisplay.MainApp/gen/de/haw_hamburg/informatik/remote_display/mainapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package de.haw_hamburg.informatik.remote_display.mainapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
